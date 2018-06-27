456407370a6d3c51cc051147b5bbb5670dd048ed
yancarlos Solarte <ycsolarte8@misena.edu.co>
2018-06-27 08:36:47 -0500

    --

--- index.php
+++ index.php
@@ -4,18 +4,19 @@ class person{
     var $firstName;
     var $lastName;
 
+    function __construct($firstName, $lastName)
+    {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+    }
+
     function fullName(){
         return $this->firstName.' '.$this->lastName;
     }
 }
 
-$person1 = new person;
-$person1->firstName='Pedro';
-$person1->lastName='Peréz';
-
-$person2 = new person;
-$person2->firstName='Pablo';
-$person2->lastName='Ramirez'; 
+$person1 = new person('Juan','Salazar');
+$person2 = new person('Cristian','Ramirez');
 
 echo "{$person1->fullName()} es amigo de {$person2->fullName()}";
 
